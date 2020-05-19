29c37c274fe24b35787ff58e549a6b99f3c0011b
pauladarias <pauladarias@gmail.com>
2020-05-19 18:01:21 +0100

    first test passed

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'sinatra/base'
 
 class User < Sinatra::Base
+
+  get '/' do
+    'hello!'
+  end 
+  
   run! if app_file == $0
 end
--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -1,3 +1,7 @@
-describe 'feature' do
+feature 'feature' do
+  scenario 'says hello' do 
+    visit('/')
+    expect(page).to have_content('hello!')
+  end 
   
 end
