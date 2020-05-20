2a6430deb4e6bf82a55fdf174bede0ed45446891
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-05-20 11:25:51 +0100

    Added create user method to add user to database

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,20 @@
 class User
   attr_reader :first_name, :last_name, :email, :password
-  
+
   def initialize(first_name, last_name, email, password)
     @first_name = first_name
     @last_name = last_name
     @email = email
     @password = password
+  end
+
+  def self.create(first_name:, last_name:, email:, password:)
+    if ENV['RACK_ENV'] = 'test'
+      connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb')
+    end
+
+    connection.exec("INSERT INTO users (first_name, last_name, email, password) VALUES('#{first_name}', '#{last_name}', '#{email}', '#{password}') RETURNING first_name, last_name, email, password")
   end
 end
