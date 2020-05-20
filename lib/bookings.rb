e8dad408b47bcacc79a58f30b015c13b34738148
tracht <marija.trachtenberg@gmail.com>
2020-05-20 21:53:54 +0100

    fixed webhelper; created dummydata.sql; unfinished #isBookable implementation

--- lib/bookings.rb
+++ lib/bookings.rb
@@ -1,3 +1,43 @@
+require 'pg'
+
 class Bookings
+
+  def initialize(date, spaceid, userid)
+    @date = date
+    @spaceid = spaceid
+    @userid = userid
+  end
+
+  def self.isBookable(owner_id, space_id, date)
+    spaceID = space_id
+    ownerID = owner_id
+    space_date = date
+
+    con = PG.connect :dbname => 'makersbnb_test', :user => 'postgres'#, :password => 'Pg5429671'
+    # the owner gets a request for a particular space
+    # return from the Request table the request details
+    space_info = con.exec("SELECT * FROM Requests WHERE spaceID = #{spaceID} AND date = #{space_date}").first
+    owner_space_id = space_info['ownerID'].to_i
+
+
+  end
+
+
+  # def self.all(owner_id)
+  #   bookings = []
+  #   bookings_mock = [ "space id = 2, 2001-01-01","space id = 2, 2001-01-02", "space id = 2, 2001-01-03" ]
+  #
+  #   con = PG.connect :dbname => 'makersbnb_test', :user => 'postgres'#, :password => 'Pg5429671'
+  #   puts space_bookings = con.exec("SELECT * FROM Spaces WHERE userid = #{owner_id}")
+  #
+  #   space_bookings.each do |row|
+  #     bookings << Bookings.new(row['date'], row['spaceID'], row['userID'])
+  #   end
+  #
+  #   # owner_space_id = space_data['spaceid'].to_i
+  #   # booking_data = con.exec("SELECT * FROM Bookings WHERE spaceid = #{owner_space_id}").first
+  #   # p booking_date = booking_data['date'].to_s
+  #   # "2001/01/01 1 1"
+  # end
 
 end
--- spec/unit/bookings_spec.rb
+++ spec/unit/bookings_spec.rb
@@ -1,19 +1,27 @@
-require 'Bookings'
+require 'bookings'
 
 describe Bookings do
 
-  describe '#isBookbale' do
-    context 'when Owner confirms a booking' do
-      it 'creates a booking in the Bookings table' do
+  describe '#isBookable' do
+    context 'when Owner confirms' do
+      it 'a booking is added to Bookings table' do
+        owner_id = double("owner_id")
+        owner_id = 2
         empty_tables()
-        fill_bookings_table()
-
-        expect(Bookings.all).to include('2001/01/01 1 1')
-        # expect(Bookings.all).to include('2001/01/01')
-        # expect(Bookings.all).to include(1)
-        # expect(Bookings.all).to include(1)
+        fill_request_table_with_1_request()
+        expect(Bookings.isBookable(owner_id)).to eq('2001/01/01, 1, 1')
       end
     end
   end
+
+  # describe '#self.all' do
+  #   it 'returns all of the owners confirmed bookings' do
+  #     empty_tables()
+  #     fill_bookings_table_with_3_bookings()
+  #     expect(Bookings.all(2)).to eq('2001-01-01, 2, 1')
+  #     # expect(Bookings.all(2)).to eq('2001-01-02, 2, 1')
+  #     # expect(Bookings.all(2)).to eq('2001-01-03, 2, 1')
+  #   end
+  # end
 
 end
