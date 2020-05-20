31eb4a111147b80e8405be30a4520dfd799444aa
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-05-20 11:05:54 +0100

    Added registration and listing spaces feature pages and tests

--- app.rb
+++ app.rb
@@ -2,8 +2,26 @@ require 'sinatra/base'
 
 class Makersbnb < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
+  end
+
+  post '/user' do
+    redirect '/user/new'
+  end
+
+  get '/user/new' do
+    erb(:sign_up)
+  end
+
+  post '/user/new' do
+    redirect '/spaces'
+  end
+
+  get '/spaces' do
+    erb(:list_spaces)
   end
 
   # start the server if ruby file executed directly
