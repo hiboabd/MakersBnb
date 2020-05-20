979de255687414770b13889a0d14b1df5232ede8
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-05-20 11:13:17 +0100

    Made user object and tests pass

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -1,7 +1,11 @@
 require 'user'
 
 describe User do
-  it 'Says Bye' do
-    expect(subject.bye).to eq('Bye!')
+  let(:user) {User.new("John", "Smith", "JohnSmith@example.com","Password123")}
+  it 'has a name, email, password' do
+    expect(user.first_name).to eq "John"
+    expect(user.last_name).to eq "Smith"
+    expect(user.email).to eq "JohnSmith@example.com"
+    expect(user.password).to eq "Password123"
   end
 end
