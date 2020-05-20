a52fa9653d96d1951dc4aa173f7524b79f2cb616
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-05-20 11:25:51 +0100

    Added create user method to add user to database

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -1,4 +1,5 @@
 require 'user'
+require 'pg'
 
 describe User do
   let(:user) {User.new("John", "Smith", "JohnSmith@example.com","Password123")}
@@ -7,5 +8,15 @@ describe User do
     expect(user.last_name).to eq "Smith"
     expect(user.email).to eq "JohnSmith@example.com"
     expect(user.password).to eq "Password123"
+  end
+
+  describe '.create' do
+    it 'adds user to database' do
+      user = User.create(first_name: "John", last_name: "Smith", email: "JohnSmith@example.com", password: "Password123").first
+      expect(user['first_name']).to eq "John"
+      expect(user['last_name']).to eq "Smith"
+      expect(user['email']).to eq "JohnSmith@example.com"
+      expect(user['password']).to eq "Password123"
+    end
   end
 end
