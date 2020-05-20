c2379cff09c8d5c54808083d62c574f28d5f71c8
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-05-20 11:13:17 +0100

    Made user object and tests pass

--- lib/user.rb
+++ lib/user.rb
@@ -1,5 +1,10 @@
 class User
-  def bye
-    'Bye!'
+  attr_reader :first_name, :last_name, :email, :password
+  
+  def initialize(first_name, last_name, email, password)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @password = password
   end
 end
