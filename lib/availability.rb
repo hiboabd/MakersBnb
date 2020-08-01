5b64250a22f2b828c0a2653a71a645164bfac1ad
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-08-01 11:12:28 +0100

    Added all and retrieve availability funciton to pass tests

--- lib/availability.rb
+++ lib/availability.rb
@@ -35,4 +35,18 @@ class Availability
     return availability_array
   end
 
+  def self.all
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test', :user => 'postgres', password: 'Pg5429671')
+    else
+      connection = PG.connect(dbname: 'makersbnb', :user => 'postgres', password: 'Pg5429671')
+    end
+
+    result = connection.exec ('SELECT * FROM availabilities;')
+    availabilities_array = result.map do |result|
+      Availability.new(result['availabilityid'], result['date'], result['spaceid'])
+    end
+    return availabilities_array
+  end
+
 end
