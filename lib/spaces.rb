3e51612a90fa450f350a48a4fcb2c210f20d5a4e
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-08-01 17:48:57 +0100

    Tests pass - added find function

--- lib/spaces.rb
+++ lib/spaces.rb
@@ -1,23 +1,24 @@
 class Spaces
 
-  attr_reader :name, :description, :price, :userID
+  attr_reader  :id, :name, :description, :price, :userID
 
-  def initialize(name, description, price, userID)
+  def initialize(id, name, description, price, userID)
+    @id = id
     @name = name
     @description = description
     @price = price
     @userID = userID
   end
 
-  def self.add_space(name:,description:,price:, userID:)
+  def self.add_space(name:, description:, price:, userID:)
     if ENV['RACK_ENV'] == 'test'
       connection = PG.connect(dbname: 'makersbnb_test', :user => 'postgres', password: 'Pg5429671')
     else
       connection = PG.connect(dbname: 'makersbnb', :user => 'postgres', password: 'Pg5429671')
     end
 
-    result = connection.exec("INSERT INTO Spaces (name, description, price, userID) VALUES ('#{name}', '#{description}', '#{price}', '#{userID}') RETURNING name, description, price, date, userID")
-    Spaces.new(result[0]['name'], result[0]['description'], result[0]['price'], result[0]['userID'])
+    result = connection.exec("INSERT INTO Spaces (name, description, price, userID) VALUES ('#{name}', '#{description}', '#{price}', '#{userID}') RETURNING spaceid, name, description, price, date, userID")
+    Spaces.new(result[0]['spaceid'], result[0]['name'], result[0]['description'], result[0]['price'], result[0]['userID'])
   end
 
   def self.all
@@ -29,8 +30,19 @@ class Spaces
 
     result = connection.exec ('SELECT * FROM Spaces;')
     spaces_array = result.map do |result|
-      Spaces.new(result['name'], result['description'], result['price'], result['userID'])
+      Spaces.new(result['spaceid'], result['name'], result['description'], result['price'], result['userID'])
     end
     return spaces_array
+  end
+
+  def self.find(id)
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test', :user => 'postgres', password: 'Pg5429671')
+    else
+      connection = PG.connect(dbname: 'makersbnb', :user => 'postgres', password: 'Pg5429671')
+    end
+
+    result = connection.exec ("SELECT * FROM Spaces WHERE spaceid = '#{id}' ")
+    Spaces.new(result[0]['spaceid'], result[0]['name'], result[0]['description'], result[0]['price'], result[0]['userID'])
   end
 end
